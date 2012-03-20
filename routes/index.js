c68418ee2d476d063a3ad55debe8c8b411b7dfc2
zziuni <zziuni@gmail.com>
2012-03-20 18:26:11 +0900

    routing test

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.show = function( req, res ){
+//    res.render( 'show', {title: 'show'} );
+    res.send('showId:' + req.params.showId);
+}

0528d779229bcd63d738dbbcaf2ffe86d5a1e4e7
zziuni <zziuni@gmail.com>
2012-03-21 08:31:28 +0900

    successing test jqptl

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
-  app.register( ".html", require( "jqtpl" ).express );
+
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-
+app.register( ".html", require( "jqtpl" ).express );
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -31,8 +31,9 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.get('/shows/:showId?', routes.show);
+app.get('/shows/:showId', routes.show);
 app.get('/shows/', routes.shows);
+app.get('/getShow/', routes.getShow);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,25 @@
 /*
  * GET home page.
  */
+var jqtpl = require('jqtpl');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' })
 };
 
 exports.show = function( req, res ){
-    res.render( 'show', {title: 'show'} );
-//    var showId = req.params.showId;
-//    res.send('showId:' + showId);
+    res.render( 'show', {title: 'show page', name: 'this name'} );
 };
 
 exports.shows = function(req, res){
-    res.send('shows page');
+//    res.send('shows page', {title: 'show Page'});
+    res.render( 'show.html', {title: 'show'} );
+}
+
+exports.getShow = function(req, res){
+    var tpl = '<div>${name}</div>';
+    jqtpl.template('getShow', tpl);
+    var result = jqtpl.tmpl('getShow', {name: 12333333});
+    res.send(result);
+
 }
